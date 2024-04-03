5029f34bfe8783a01b504b4dff36e9c0fbadb9c9
Lucas Marcos <lucasmarcos@live.com>
2024-04-03 14:28:36 -0300

    reorganizacao do app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
 import express from "express";
-import routes from "./routes/quizRouter";
+
+import cors from "cors";
+
 import quizRouter from "./routes/quizRouter";
 import userRouter from "./routes/userRouter";
 import perguntaRouter from "./routes/perguntaRouter";
-import cors from "cors";
 import pontuacacaoRouter from "./routes/pontuacaoRouter";
 
 const app = express();
-app.use(cors());
-const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/quizzes", quizRouter);
 app.use("/user", userRouter);
 app.use("/pergunta", perguntaRouter);
 app.use("/pontuacao", pontuacacaoRouter);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
