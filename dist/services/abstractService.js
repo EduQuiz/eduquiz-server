bbc9f7124d783e3f28b5058dbac98d54440c89f0
Lucas Marcos <lucasmarcos@live.com>
2024-04-03 14:17:20 -0300

    remover artefatos de compilacao do repositorio

--- dist/services/abstractService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AbstractService = void 0;
-class AbstractService {
-}
-exports.AbstractService = AbstractService;
