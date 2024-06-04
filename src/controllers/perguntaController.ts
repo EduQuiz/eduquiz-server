794fc9d2f55c6ae38d2257287232043607f24e99
Lucas Marcos <lucasmarcos@live.com>
2024-06-04 18:48:45 -0300

    console.error and status code

--- src/controllers/perguntaController.ts
+++ src/controllers/perguntaController.ts
@@ -13,19 +13,18 @@ export const postPergunta = async (req: Request, res: Response) => {
   try {
     const { pergunta, alternativas } = req.body;
     const resp = await novaPergunta(pergunta, alternativas);
-    return res.status(200).json(resp);
+    return res.status(201).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
 export const getAllPerguntas = async (req: Request, res: Response) => {
   try {
-    console.log("getAllPerguntas, no body e noparams:");
     const resp = await findAllPerguntas();
     return res.status(200).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
@@ -34,12 +33,11 @@ export const getAllPerguntasWithIdQuiz = async (
   res: Response,
 ) => {
   try {
-    console.log("getAllPerguntasWithIdQuiz, params.id:");
-    const idquiz = req.params.quizId;
-    const resp = await findAllPerguntasWithQuizId(idquiz);
+    const { id } = req.params;
+    const resp = await findAllPerguntasWithQuizId(id);
     return res.status(200).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
@@ -49,7 +47,7 @@ export const getOnePergunta = async (req: Request, res: Response) => {
     const resp = await encontrarPergunta(id);
     return res.status(200).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(201).json({});
   }
 };
@@ -60,18 +58,17 @@ export const deletePergunta = async (req: Request, res: Response) => {
     const resp = await removerPergunta(id);
     return res.status(200).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(201).json({});
   }
 };
 
 export const updatePergunta = async (req: Request, res: Response) => {
   try {
-    console.log("updatePergunta,  body:", req.body);
     const data: Pergunta = req.body;
     const resp = await upersetPergunta(data);
     return res.status(200).json(resp);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
